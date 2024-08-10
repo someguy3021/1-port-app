2787c040fb5588e6c51e5ae4480de9e9e4ea06a7
someguy3021 <162592428+someguy3021@users.noreply.github.com>
2024-08-10 23:10:07 +0500

    2
    2

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -1,19 +1,11 @@
 <template>
-  <q-item
-    clickable
-    tag="a"
-    target="_blank"
-    :href="props.link"
-  >
-    <q-item-section
-      v-if="props.icon"
-      avatar
-    >
+  <q-item clickable tag="a" target="_blank" :href="props.link">
+    <q-item-section v-if="props.icon" avatar>
       <q-icon :name="props.icon" />
     </q-item-section>
 
     <q-item-section>
-      <q-item-label>{{ props.title }}</q-item-label>
+      <q-item-label>{{ props.title }} 1</q-item-label>
       <q-item-label caption>{{ props.caption }}</q-item-label>
     </q-item-section>
   </q-item>
@@ -21,28 +13,28 @@
 
 <script setup>
 defineOptions({
-  name: 'EssentialLink'
-})
+  name: "EssentialLink",
+});
 
 const props = defineProps({
   title: {
     type: String,
-    required: true
+    required: true,
   },
 
   caption: {
     type: String,
-    default: ''
+    default: "",
   },
 
   link: {
     type: String,
-    default: '#'
+    default: "#",
   },
 
   icon: {
     type: String,
-    default: ''
-  }
-})
+    default: "",
+  },
+});
 </script>
