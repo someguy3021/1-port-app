34e746d50e4b34074713343cb497554521f510b8
someguy3021 <162592428+someguy3021@users.noreply.github.com>
2024-08-13 01:11:47 +0500

    1
    1

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -5,7 +5,6 @@
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
     />
-    {{ $t("failed") }}
     <q-btn :label="$t('failed')" />
   </q-page>
 </template>
