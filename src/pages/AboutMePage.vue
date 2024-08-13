bb004ae74fd09567497ea06447d30d189c705cd7
someguy3021 <162592428+someguy3021@users.noreply.github.com>
2024-08-13 22:52:41 +0500

    AboutMe Placeholders

--- src/pages/AboutMePage.vue
+++ src/pages/AboutMePage.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page class="">
     <div :class="$q.dark.isActive ? 'bg-gr1-darktheme' : 'bg-gr1-lighttheme'">
-      <div class="container">
+      <div class="whoiam container">
         <div>
           <div>
             <div>FrontEnd Developer\Designer.</div>
@@ -26,8 +26,12 @@
       </div>
     </div>
     <div>
-      <div class="container bg-glass-black border-2px-radius50">
-        321312321132131dsadasdasdsadasdasdsa d3d123d12d21d1
+      <div class="myworksexample container bg-glass-black border-2px-radius50">
+        <img
+          alt="Quasar logo"
+          src="~assets/quasar-logo-vertical.svg"
+          style="width: 200px; height: 200px"
+        />
         <img
           alt="Quasar logo"
           src="~assets/quasar-logo-vertical.svg"
@@ -36,13 +40,25 @@
       </div>
     </div>
     <div :class="$q.dark.isActive ? 'bg-gr2-darktheme' : 'bg-gr2-lighttheme'">
-      <div class="container">
-        321312321132131dsadasdasdsadasdasdsa d3d123d12d21d1
-        <img
-          alt="Quasar logo"
-          src="~assets/quasar-logo-vertical.svg"
-          style="width: 200px; height: 200px"
-        />
+      <div class="myskills container">
+        <div>
+          <div>Design</div>
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
+            voluptatem atque, incidunt ipsam porro, expedita sapiente sit
+            voluptatibus ducimus provident error repellendus magnam! Ipsa
+            obcaecati blanditiis porro voluptates. Eius, assumenda.
+          </div>
+        </div>
+        <div>
+          <div>FrontEnd</div>
+          <div>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo
+            minima voluptas temporibus, pariatur quae praesentium qui itaque
+            numquam molestiae distinctio maiores impedit consequatur modi
+            voluptate mollitia veritatis nihil alias nesciunt!
+          </div>
+        </div>
       </div>
     </div>
   </q-page>
@@ -59,7 +75,7 @@ defineOptions({
   background: linear-gradient(to left, $primary, white, white, $accent);
 }
 .bg-gr1-darktheme {
-  background: linear-gradient(to left, $primary, $accent);
+  background: linear-gradient(to left, $dark-page, $accent);
 }
 
 .bg-glass-black {
