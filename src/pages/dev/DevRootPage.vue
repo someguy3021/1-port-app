e9476b4dd0a4d0d035c905730ca0d29702fdd37a
someguy3021 <162592428+someguy3021@users.noreply.github.com>
2025-02-13 19:05:18 +0500

    upd

--- src/pages/dev/DevRootPage.vue
+++ src/pages/dev/DevRootPage.vue
@@ -12,8 +12,11 @@
               :class="$q.screen.lt.sm ? 'justify-around' : 'justify-between'">
               <div class="q-pr-md text-h4 text-center q-pt-sm">{{ $t("devPages_devRootPage_me_name") }}</div>
               <div class="q-pl-md flex">
-                <q-btn disable round color="dark" icon="fa-brands fa-telegram" size="lg" />
-                <q-btn disable round color="dark" icon="fa-brands fa-github" size="lg" />
+                <q-btn class="q-mx-sm" disable round color="dark" icon="fa-brands fa-telegram" size="lg" />
+                <q-btn class="q-mx-sm" disable round color="dark" icon="fa-brands fa-github" size="lg" />
+                <q-tooltip class="bg-negative text-black text-body2 shadow-alwaysBlack-20 q-pa-sm" :offset="[10, 10]">{{
+                  $t("not_yet_available") }}
+                </q-tooltip>
               </div>
             </q-card-section>
             <q-separator color="accent" size='2px' />
